8499334dbeab91a67aadfb2b7993277aedc47eec
Jason Dent <jason@streetsidesoftware.nl>
2026-09-15 08:06:26 +0200

    fix: Add samples

---
+++ packages/parser-typescript/src/recommended.ts
@@ -0,0 +1,11 @@
+import { plugin as typeScriptPlugin } from './plugin.js';
+
+export default {
+  plugins: [typeScriptPlugin],
+  languageSettings: [
+    {
+      languageId: 'typescript,javascript,typescriptreact,javascriptreact',
+      parser: 'typescript'
+    }
+  ]
+};
